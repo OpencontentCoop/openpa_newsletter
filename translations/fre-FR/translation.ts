1baae89cf99892abf22a067249baca694cca2ecb
Luca Realdi <luca.realdi@opencontent.it>
2025-06-03 11:44:05 +0200

    Add fre-FR translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+  <context>
+    <name>openpa_newsletter</name>
+    <message>
+      <source>Enter your email</source>
+      <translation>Saisir votre adresse électronique</translation>
+    </message>
+    <message>
+      <source>Subscribe</source>
+      <translation>Inscrivez-vous</translation>
+    </message>
+    <message>
+      <source>Unsubscribe</source>
+      <translation>Se désinscrire</translation>
+    </message>
+    <message>
+      <source>I certify that I have read and accept the %privacy_link_start%Privacy Statement%privacy_link_end%</source>
+      <translation>Je déclare avoir pris connaissance de la %privacy_link_start%politique de confidentialité sur le traitement des données personnelles%privacy_link_end%</translation>
+    </message>
+    <message>
+      <source>Registration successful!</source>
+      <translation>Inscription effectuée avec succès !</translation>
+    </message>
+    <message>
+      <source>Error processing your request: contact support</source>
+      <translation>Erreur lors du traitement de la demande : contacter le support</translation>
+    </message>
+    <message>
+      <source>Error processing the request for subscription to the list</source>
+      <translation>Erreur lors du traitement de la demande d’inscription à la liste</translation>
+    </message>
+    <message>
+      <source>There are problems with the email address entered: at this moment the address is in status %status%</source>
+      <translation>Il y a des problèmes avec l’adresse e-mail saisie : en ce moment, l’adresse est en état %status%</translation>
+    </message>
+    <message>
+      <source>The address is already registered on the list</source>
+      <translation>L’adresse est déjà inscrite sur la liste</translation>
+    </message>
+    <message>
+      <source>At least one list must be selected</source>
+      <translation>Vous devez sélectionner au moins une liste</translation>
+    </message>
+    <message>
+      <source>Campaign created successfully: now you can send the newsletter from %sendy_api_url%. After sending it, click on the 'Mark as sent' button to archive this newsletter</source>
+      <translation>Campagne créée avec succès : vous pouvez désormais envoyer la newsletter depuis %sendy_api_url%. Après l’avoir envoyée, cliquer sur le bouton « Marquer comme envoyée »</translation>
+    </message>
+  </context>
+  <context>
+    <name>cjw_newsletter/datatypes</name>
+    <message>
+      <source>CJW Newsletter Edition</source>
+      <translation>Édition de la newsletter CJW</translation>
+    </message>
+  </context>
+</TS>
